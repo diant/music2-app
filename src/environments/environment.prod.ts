a2ddc5c0d376e03edba8f19824c7959bd3270f5e
diant <diant@live.com>
2017-03-10 14:22:29 +0100

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true
 };
+
+export const target = 'PROD 1.2';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,3 +6,5 @@
 export const environment = {
   production: false
 };
+
+export const target = 'DEV 1.0';
